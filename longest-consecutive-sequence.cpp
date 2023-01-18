44314a865b6d31c15f0e00c6dce19ee352161904
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-17 19:26:40 -0800

    Completed

--- longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <set>
+#include <cstdlib>
 
 using namespace std;
 
+int longestConsecutive(vector<int>& nums) {
+  int sequenceLength = 0;
+  int longestSequenceLength = 0;
+  int prevNumber;
+
+  set<int, greater<int> > s;
+  set<int, greater<int> >::iterator itr;
+
+  // insert nums in to set s
+  for (int num : nums)
+    s.insert(num);
+
+  // iterating through s to see if there is a sequence
+  for (itr = s.begin(); itr != s.end(); itr++) {
+    // cout << *itr << "-";
+    if (itr == s.begin()) {
+      sequenceLength = 1;
+      longestSequenceLength = 1;
+
+    }
+    else {
+      if ((prevNumber - *itr) == 1) {
+        sequenceLength++;
+      } else {
+        if (sequenceLength > longestSequenceLength)
+          longestSequenceLength = sequenceLength;
+        sequenceLength = 1;
+      }
+    }
+
+    prevNumber = *itr;
+
+    // cout << "sl-" << sequenceLength << " ";
+
+  }
+
+  // cout << "\n";
+
+  if (sequenceLength > longestSequenceLength)
+    longestSequenceLength = sequenceLength;
+
+  return longestSequenceLength;
+}
+
 int main(int argc, char const *argv[]) {
+  vector<int> nums = {9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6};
+
+  cout << longestConsecutive(nums) << "\n";
   return 0;
 }
